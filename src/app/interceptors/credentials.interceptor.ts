c257e84b667cee3877c45732875c94fb656081fe
RafaelEmanas <rafael.emanueldv.pro@gmail.com>
2026-03-18 14:56:16 -0400

    fix: skip credentials interceptor for R2 storage
    - Bypass withCredentials for R2 public endpoint requests
    - Prevents CORS issues when uploading to object storage

--- src/app/interceptors/credentials.interceptor.ts
+++ src/app/interceptors/credentials.interceptor.ts
@@ -1,6 +1,12 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../../environment';
 
 export const credentialsInterceptor: HttpInterceptorFn = (req, next) => {
+
+  if (req.url.startsWith(environment.r2PublicEndpoint)) {
+    return next(req);
+  }
+
   const clonedReq = req.clone({
     withCredentials: true
   });
