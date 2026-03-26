93d0b8ee532feb6c6cd67476a6660c553515bec8
RafaelEmanas <rafael.emanueldv.pro@gmail.com>
2026-03-26 12:02:25 -0400

    refactor(home): inline page title template
    - Remove home-page-title component files
    - Inline template directly in home.html
    - Remove HomePageTitle import from home.ts

--- src/app/components/home-page-title/home-page-title.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'page-title-component',
-  imports: [],
-  templateUrl: './home-page-title.html',
-})
-export class HomePageTitle {}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,13 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { HomePageTitle } from '../../components/home-page-title/home-page-title';
 import { HomeSearchbar } from '../../components/home-searchbar/home-searchbar';
 import { HomeItemsList } from '../../components/home-items-list/home-items-list';
 import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'home-page',
-  imports: [HomePageTitle, HomeSearchbar, HomeItemsList, Header],
+  imports: [HomeSearchbar, HomeItemsList, Header],
   templateUrl: './home.html',
 })
 export class Home {
